f6b9ff4b3d4e5c2b2710df725c5f4fc7ff76bab2
varsha Gunjal <varshashingote01@gmail.com>
2023-02-01 12:32:05 +0530

    [varsha] Dictionary Collection

---
+++ Col4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Col4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String,object> dt=new Dictionary<String,object>();
+            dt.Add("Eid", 001);
+            dt.Add("Ename", "varsha");
+            dt.Add("Eno", 877745435);
+
+            foreach(String key in dt.Keys)
+            {
+                Console.WriteLine(key + ":" + dt[key]);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
